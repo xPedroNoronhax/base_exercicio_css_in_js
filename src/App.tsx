621c19d7c358bc7490d85ec28b57770161503cc3
Pedro Noronha <pedro_noronha@outlook.com>
2023-12-04 10:52:47 -0300

    ajustando <Container>

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import { Container } from './styles'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
+      {/* <div className="container">
+        <ListaVagas />
+      </div> */}
     </>
   )
 }
